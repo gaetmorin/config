477141c8b7a9cb6b6fdd31a6240ffb69ad4ebdfe
Gaëtan Morin <gaet.morin@gmail.com>
2015-09-12 01:33:13 +0200

    zsh: automatically add local/opt/*/bin directories to PATH

--- zsh/2-env.zsh
+++ zsh/2-env.zsh
@@ -9,6 +9,13 @@
 # 256 color terminal, where available
 export TERM=xterm-256color
 
+# Add bin directories of local installs to PATH
+for dir in $HOME/local/opt/*/bin; do
+    if test -d $dir; then
+        PATH=$dir:$PATH
+    fi
+done
+
 # Local paths
 export PATH=$HOME/bin:$HOME/local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
@@ -16,4 +23,4 @@ export PYTHONPATH=$HOME/local/lib/python:$PYTHONPATH
 
 # Go path
 export GOPATH=$HOME
-export GOROOT=$HOME/local/go
+export GOROOT=$HOME/local/opt/go
